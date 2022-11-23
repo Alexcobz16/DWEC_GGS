c1a4b8fc335dfe4f39564f47ade85fcdd79771cb
Alexcobz16 <alejandrocoronabenitez@gmail.com>
2022-11-23 16:40:15 +0100

    find y filter

--- Tema2/filter.js
+++ Tema2/filter.js
@@ -11,20 +11,21 @@
 //     return valor%2 == 0;
 // }
 
-function find(array, callback){
-    res = null;
-    for(i=0;i<array.length && !res;i++){
-        if(callback(array[i], i, array)){
-            res = array[i];
-        }
-    }
-}
+// function find(array, callback){
+//     res = null;
+//     for(i=0;i<array.length && !res;i++){
+//         if(callback(array[i], i, array)){
+//             res = array[i];
+//         }
+//     }
+//     console.log(res);
+// }
 
-function primerIgual(){
-    for(i;i<array.length;i++){
-        
-    }
-}
+// function primerIgual(valor, posicion, array){
+//     for(posicion;posicion<array.length;posicion++){
+//         return valor == array[posicion];
+//     }
+// }
 
-let array = [3,6,9,7,7,4,9];
-find(array, primerIgual);
+// let array = [3,6,9,7,7,4,9];
+// find(array, primerIgual);
