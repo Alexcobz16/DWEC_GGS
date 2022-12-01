b616115ac6d545f8b3d96f6801996b0102cd4d1a
Alexcobz16 <alejandrocoronabenitez@gmail.com>
2022-12-01 19:55:43 +0100

    prueba suerte

---
+++ Tema3/Acts.js
@@ -0,0 +1,26 @@
+// function anadirPrefijo(prefijo, ...palabras){
+//     const palabrasConPrefijo = [];
+//     for(let i=0;i<palabras.length;i++){
+//         palabrasConPrefijo[i] = prefijo + palabras[i];
+//     }
+//     return palabrasConPrefijo;
+// }
+
+// añadirPrefijo("con", "verso", "vexo", "cavo");
+
+// const saludo = function(){
+//     return "¡Hola!";
+// };
+
+// const saludoConNombre = function (nombre) {
+//     return 'Hola ' + nombre;
+// };
+
+// const sumar = function(a, b){
+//     return a+b;
+// };
+
+// console.log(saludo());
+// console.log(saludoConNombre('calvo'));
+// console.log(sumar(2,2));
+
