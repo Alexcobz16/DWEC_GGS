33674a67a21e5c550eb1cdbd509c1a3894c2d18d
Alexcobz16 <alejandrocoronabenitez@gmail.com>
2022-12-02 19:17:35 +0100

    coche a medio hacer

--- Tema3/Acts.js
+++ Tema3/Acts.js
@@ -23,4 +23,50 @@
 // console.log(saludo());
 // console.log(saludoConNombre('calvo'));
 // console.log(sumar(2,2));
+class Coche {
 
+    #fabricante = '';
+    #modelo = '';
+    #marchaActual = "P";
+
+    static marchas(){
+        const MARCHAS = ["P",1,2,3,4,5,6,"R"];
+        return MARCHAS;
+    }
+
+    constructor(fabricante, modelo) {
+        this.#fabricante = fabricante;
+        this.#modelo = modelo;
+    }
+
+    cambiarMarcha(input){
+        let res = '';
+        res = this.marchas.filter(marcha => input == marcha);
+        if(res == ''){
+            this.marchaActual = "P";
+        }else{
+            this.marchaActual = res;
+        }
+    }
+
+    get modelo(){
+        return this.#modelo;
+    }
+    
+    get fabricante(){
+        return this.#fabricante;
+    }
+    
+    set fabricante(fabricante){
+        this.#fabricante = fabricante;
+    }
+
+    set modelo(modelo){
+        this.#modelo = modelo;
+    }
+    
+}
+
+let coche = new Coche("Honda", "Civic");
+coche.cambiarMarcha(27);
+console.log(coche.marchaActual);
