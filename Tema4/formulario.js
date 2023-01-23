8edcd53bf17ae8aedeca0db927f06a00a724875d
Alexcobz16 <alejandrocoronabenitez@gmail.com>
2023-01-23 18:52:00 +0100

    otra prueba mas

--- Tema4/formulario.js
+++ Tema4/formulario.js
@@ -1,14 +1,9 @@
-document.getElementById("enviar").addEventListener("click", comprobar, false);
 
-function comprobar(){
-    let email = document.getElementsByClassName("required")[0];
-    let psswd = document.getElementsByClassName("required")[1];
-    
-    if(email.value == ""){
-        email.className("invalid");
-    }
-    if(psswd.value == ""){
-        psswd.value = "Falta por rellenar";
-        
-    }
+
+function inhabilitar(){
+    document.forms[0]["enviar"].disabled = true;
+}
+
+function habilitar(){
+    document.forms[0]["enviar"].disabled = false;
 }
