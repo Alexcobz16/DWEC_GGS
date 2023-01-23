1e9cfb6a298537c8d0d2ba614797d8c300c696b6
Alexcobz16 <alejandrocoronabenitez@gmail.com>
2023-01-23 17:35:15 +0100

    controlando formularios con el js

--- Tema4/formulario.js
+++ Tema4/formulario.js
@@ -1,2 +1,14 @@
-let form = document.getElementById("formulario").firstElementChild;
-console.log(form.textContent);
+document.getElementById("enviar").addEventListener("click", comprobar, false);
+
+function comprobar(){
+    let email = document.getElementsByClassName("required")[0];
+    let psswd = document.getElementsByClassName("required")[1];
+    
+    if(email.value == ""){
+        email.className("invalid");
+    }
+    if(psswd.value == ""){
+        psswd.value = "Falta por rellenar";
+        
+    }
+}
