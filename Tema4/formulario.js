20a310b8d6435a664e9da57173a500e706a67209
Alexcobz16 <alejandrocoronabenitez@gmail.com>
2023-01-27 20:35:07 +0100

    mas pruebas

--- Tema4/formulario.js
+++ Tema4/formulario.js
@@ -2,11 +2,8 @@ document.forms[0]["email"].addEventListener("blur", email, false);
 
 function email(){
     if(!document.forms[0]["email"].checkValidity()){
-        
-        comprobarErrores();
-
         document.forms[0]["email"].className = "invalid";
-        document.getElementById("errEmail").textContent = "Hay errores crack";
+        document.getElementById("errEmail").textContent = comprobarErrores(document.forms[0]["email"]);
         inhabilitar();
     }else{
         document.forms[0]["email"].className = "required";
@@ -16,8 +13,26 @@ function email(){
     }
 }
 
-function comprobarErrores(){
-    
+function comprobarErrores(input){
+    let error = "";
+
+    if(input.validity.valueMissing) {
+        error += "Este campo no puede estar vacío."
+    }
+    // El contenido del campo no es el que especifica el tipo de campo
+    if(input.validity.typeMismatch) {
+        error += "Los datos suministrados no tienen el formato correcto";
+    }
+    // El rango de valores no está entre los especificados
+    if(input.validity.rangeUnderflow || input.validity.rangeOverflow) {
+        error += `Debe contener un valor entre ${input.min} y ${input.max}`;
+    }
+    // No se cumple el patrón definido en el campo
+    if(input.validity.patternMismatch) {
+        error += "El campo debe contener al menos un número";
+    }
+
+    return error;
 }
 
 function inhabilitar(){
