087a3c18043811c68a3f18ad9e7a0c35f928b471
Alexcobz16 <alejandrocoronabenitez@gmail.com>
2023-01-27 17:56:00 +0100

    grandes avances en validar

--- Tema4/formulario.js
+++ Tema4/formulario.js
@@ -1,4 +1,24 @@
+document.forms[0]["email"].addEventListener("blur", email, false);
 
+function email(){
+    if(!document.forms[0]["email"].checkValidity()){
+        
+        comprobarErrores();
+
+        document.forms[0]["email"].className = "invalid";
+        document.getElementById("errEmail").textContent = "Hay errores crack";
+        inhabilitar();
+    }else{
+        document.forms[0]["email"].className = "required";
+        document.getElementById("errEmail").textContent = "";
+        habilitar();
+
+    }
+}
+
+function comprobarErrores(){
+    
+}
 
 function inhabilitar(){
     document.forms[0]["enviar"].disabled = true;
