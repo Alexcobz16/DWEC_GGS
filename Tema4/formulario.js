caa4ed8e983f0613addda85c1834b839ae284d95
Alexcobz16 <alejandrocoronabenitez@gmail.com>
2023-02-18 20:07:24 +0100

    ggs

--- Tema4/formulario.js
+++ Tema4/formulario.js
@@ -16,23 +16,29 @@ function validarElemento(e) {
 }
 
 function validarEmail(email){
+
     const regexp = new RegExp("[A-Z a-z ñ Ñ 0-9]*@[A-Z a-z ñ Ñ 0-9]*\.[A-Z a-z ñ Ñ 0-9]{0,5}");
-    let errores;
+    let errores = "";
     let mostrar = false;
-    if(!validarRegExp(email, regexp)){
-        errores += "El email debe tener @ y . para ser válido. ";
-        mostrar = true;
-    }else if(email.checkValidity()){
-        errores += validar(email);
-        mostrar = true;
+
+    if(!validarRegExp(email, regexp) || email.checkValidity()){
+        if(!validarRegExp(email, regexp)){
+            errores += "El email debe tener @ y . para ser válido. ";
+            mostrar = true;    
+        }
+        if(email.checkValidity()){
+            errores += validar(email);
+            mostrar = true;    
+        }
     }else{
         mostrar = false;
     }
-
+    
     if(mostrar){
         document.getElementById("errEmail").textContent = errores;
     }else{
         document.getElementById("errEmail").textContent = "";
+        errores = "";
     }
 }
 
@@ -55,8 +61,8 @@ function validarNombre(nombre){
 }
 
 function validarEdad(edad){
-    if(nombre.checkValidity()){
-        errores += validar(nombre);
+    if(edad.checkValidity()){
+        errores += validar(edad);
         document.getElementById("errEdad").textContent = errores;
     }else{
         document.getElementById("errEdad").textContent = "";
@@ -68,7 +74,18 @@ function validarRegExp(input, regexp){
 }
 
 function validar(input){
-    if(input.validity.patternMismatch){
-
+    let error = "";
+    if(input.validity.valueMissing) {
+        error += "Este campo no puede estar vacío.";
     }
+    if(input.validity.typeMismatch) {
+        error += "Los datos suministrados no tienen el formato correcto";
+    }
+    if(input.validity.rangeUnderflow || input.validity.rangeOverflow) {
+        error += "Debe contener un valor entre " + input.min + " y " + input.max;
+    }
+    if(input.validity.patternMismatch) {
+        error += "El campo debe contener al menos un número";
+    }
+    return error;
 }
